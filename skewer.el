5c168610ded1c660e7119df52c0b4d5698a91a98
Christopher Wellons <mosquitopsu@gmail.com>
2012-10-21 20:15:14 -0400

    Eval last expression.

--- skewer.el
+++ skewer.el
@@ -15,5 +15,27 @@
           (insert string))
       (error nil))))
 
+(defun skewer-eval-last-expression ()
+  "Evaluate the JavaScript expression before the point in the
+waiting browser."
+  (interactive)
+  (save-excursion
+    (re-search-backward "[^ \n\r]+")
+    (forward-char)
+    (let ((p (point))
+           (last (point-min)))
+      (goto-char (point-min))
+      (while (< (point) p)
+        (setq last (point))
+        (re-search-forward "[^ \n\r]")
+        (backward-char)
+        (js--forward-expression))
+      (skewer-eval (buffer-substring-no-properties last p)))))
+
+;;; Test
+
 (skewer-eval "Math.pow(35, 5)")
 (skewer-eval "$('h1').html('Foobar');")
+(skewer-eval "alert('foo')")
+(skewer-eval "function foo() { return 100; }")
+(skewer-eval "foo()")
