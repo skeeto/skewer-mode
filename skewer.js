1d006cfe4273c60ade525b457dfe6059b673a2b0
Christopher Wellons <mosquitopsu@gmail.com>
2012-10-26 23:58:02 -0400

    Use a custom printer to fix printing issues.

--- skewer.js
+++ skewer.js
@@ -1,22 +1,44 @@
 function skewer() {
+    function safeStringify(obj, seen) {
+        seen = seen || [];
+        if (obj === true) {
+            return "true";
+        } else if (obj === false) {
+            return "false";
+        } else if (obj == undefined) {
+            return "undefined";
+        } else if (typeof obj == "number") {
+            return obj.toString();
+        } else if (obj instanceof Array) {
+            if (seen.indexOf(obj) >= 0) {
+                return "[ Circular ]";
+            } else {
+                seen.push(obj);
+                return "[" + obj.map(function(e) {
+                    return safeStringify(e, seen);
+                }).join(", ") + "]";
+            }
+        } else if (typeof obj == "string") {
+            return JSON.stringify(obj);
+        } else {
+            return "Object";
+        }
+    }
+
     $.get("/skewer/get", function (str) {
         var request = JSON.parse(str);
         var result = {"id": request.id, "callback": request.callback};
-        var value;
         try {
-            value = (eval, eval)(request.eval); // global eval
+            var value = (eval, eval)(request.eval); // global eval
+            result.value = safeStringify(value);
             result.status = "success";
         } catch (error) {
-            value = error;
+            result.value = error.toString();
             result.status = "error";
             result.error = {"name": error.name, "stack": error.stack,
                             "type": error.type, "message": error.message,
                             "eval": request.eval};
         }
-        if (value == undefined)
-            result.value = "undefined";
-        else
-            result.value = value.toString(); // crush circular data
         $.post("/skewer/post", JSON.stringify(result), skewer);
     }, "text");
 }
